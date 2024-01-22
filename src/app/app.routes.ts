eb4ef65594982cbd70ac70790750e70f404576dc
Wisthong Martinez <106920956+Wisthong@users.noreply.github.com>
2024-01-22 16:42:29 -0500

    redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    title: 'Ricky&Morty',
+  },
+  {
+    path: '**',
+    component: AppComponent,
+    title: 'Ricky&Morty red***',
+  },
+];
