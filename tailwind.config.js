beac44aa3b636ab1fa7e5c8c9cd909f49efc5099
Wisthong Martinez <106920956+Wisthong@users.noreply.github.com>
2024-01-22 16:34:12 -0500

    Footer terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
+    fontFamily: {
+      martel: ["Martel", "serif"],
+      mulish: ["Mulish", "sans-serif"],
+      ptsans: ["PT Sans", "sans-serif"],
+
+      // font-family: 'Martel', serif; footer
+      // font-family: 'Mulish', sans-serif; titulo
+      // font-family: 'PT Sans', sans-serif; cuerpo
+    },
     extend: {},
   },
   plugins: [],
-}
+};
